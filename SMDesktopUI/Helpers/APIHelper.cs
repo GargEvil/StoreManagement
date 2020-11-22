ab83928db6ce8bd18bae7734b276885ad3c16e1c
Mahir Skula <mahir.skula96@hotmail.com>
2020-11-22 23:12:52 +0100

    Adding Helper class for wiring up API

---
+++ SMDesktopUI/Helpers/APIHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Configuration;
+
+namespace SMDesktopUI.Helpers
+{
+    public class APIHelper
+    {
+        //handle all API call interactions
+        public HttpClient ApiClient { get; set; }
+
+        public APIHelper()
+        {
+            InitializeClient();
+        }
+
+        private void InitializeClient()
+        {
+            string api = ConfigurationManager.AppSettings["api"];
+
+            ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri(api);
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+
+            ApiClient.DefaultRequestHeaders
+                .Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+
+        public async Task Authenticate(string username, string password)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password)
+            });
+
+            using (HttpResponseMessage response = await ApiClient.PostAsync("/Token", data))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<string>();
+                }
+            }
+        }
+    }
+}
